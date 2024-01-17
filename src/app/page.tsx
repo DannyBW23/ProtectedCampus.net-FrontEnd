1815c1fc6fcea2f30dd2567df36b9ceb00c7248d
DannyBW23 <dannybw212@gmail.com>
2024-01-17 04:05:59 -0500

    o

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Home from './home/page';
+import Head from 'next/head';
 export default function Page() {  
 
-
   return (
-    
+   <>
+<Head>
+        <meta name="viewport" content="width=device-width, initial-scale=0.8" />
+        {/* Global meta tags and links */}
+      </Head> 
 <Home/>
+</>
   );
 
 
