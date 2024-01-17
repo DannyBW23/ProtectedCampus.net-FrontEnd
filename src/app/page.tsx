07f0d438c78bc336bdb468aa437567bcd1b35054
DannyBW23 <dannybw212@gmail.com>
2024-01-17 04:12:10 -0500

    o

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
    <>
 <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=0.8" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.5" />
         {/* Global meta tags and links */}
       </Head> 
 <Home/>
