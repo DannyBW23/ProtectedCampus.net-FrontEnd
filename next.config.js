710446a5d6b70e37001a99814f011875fc34bbdf
DannyBW23 <dannybw212@gmail.com>
2024-01-17 02:06:59 -0500

    o

--- next.config.js
+++ next.config.js
@@ -1,35 +1,31 @@
 const nextConfig = {
-    images: {
-      domains: ['profilepic23.s3.amazonaws.com'],
-    },
-    reactStrictMode: true,
-  };
-  module.exports = {
-  
-    async redirects() {
-      return [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: '(www\\.)?protectedcampus\\.com',
-            },
-            {
-              type: 'protocol',
-              value: 'http',
-            },
-          ],
-          permanent: true,
-          destination: 'https://protectedcampus.com/:path*',
-        },
-      ];
-    },
-    
+  images: {
+    domains: ['profilepic23.s3.amazonaws.com'],
+  },
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '(www\\.)?protectedcampus\\.com',
+          },
+          {
+            type: 'protocol',
+            value: 'http',
+          },
+        ],
+        permanent: true,
+        destination: 'https://protectedcampus.com/:path*',
+      },
+    ];
+  },
   images: {
     loader: 'akamai',
     path: '/',
   },
 }
-  module.exports = nextConfig;
-  
+
+module.exports = nextConfig;
