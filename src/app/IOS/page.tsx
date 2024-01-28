b43da603ecb6ecd30162590d27447827187d3527
DannyBW23 <dannybw212@gmail.com>
2024-01-27 22:03:03 -0500

    o

--- src/app/IOS/page.tsx
+++ src/app/IOS/page.tsx
@@ -64,7 +64,7 @@ const selectedSchool = searchParams.get('selectedSchool');
 <div style={{alignItems: 'center',justifyContent: 'center', display: 'flex'}}>
 
 
-<Image src={"https://profilepic23.s3.amazonaws.com/Review++Thumbnail+++Headphone++Technology++Gold++Modern++Youtube+Channel+Thumbnail+(6).png"} alt="Image from S3" width= "1450" height="600"
+<Image src={"https://profilepic23.s3.amazonaws.com/ProtectedCampus.com+(1).jpg"} alt="Image from S3" width= "1450" height="600"
  />
 </div> 
 
